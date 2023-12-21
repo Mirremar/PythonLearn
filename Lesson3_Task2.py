859f28024bb3f53c65913560d0d4a0cf5a9c5419
Main Villain <deserteagle081@gmail.com>
2023-12-21 16:52:05 +0300

    task 3 sem 3

---
+++ Lesson3_Task2.py
@@ -0,0 +1,11 @@
+#Сдвинуть элементы массива на k элементов вправо
+
+import random
+nubmers = [random.randint(-10,10) for _ in range(10)]
+print(nubmers)
+offset = int(input("enter offset"))
+temp = 0
+for _ in range(offset):
+    temp = nubmers.pop()
+    nubmers.insert(0,temp)
+print(nubmers)
