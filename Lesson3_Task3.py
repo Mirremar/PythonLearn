e8d3acde8bcc0ca24c7470c6b46154cc5e5f19f2
Main Villain <deserteagle081@gmail.com>
2023-12-21 18:44:42 +0300

    Работа со словарями

---
+++ Lesson3_Task3.py
@@ -0,0 +1,10 @@
+#печать уникальных значений словарей
+input = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
+{"VI": "S005"}, {"VII": "S005"}, {" V ":"S009"}, {" VIII":"S007"}]
+values = []
+
+for dictionary in input:
+    for key in dictionary:
+        values.append(dictionary[key])
+print(set(values))
+
